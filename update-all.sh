418b4871990a53709110c3c854e329d11c27c0d2
Durk Talsma <durktals@gmail.com>
2011-09-27 08:28:20 +0200

    Added USAirways fleet info.

--- update-all.sh
+++ update-all.sh
@@ -5,11 +5,13 @@ cat fleet-info/*AAL-ac.conf American-Airlines/american_airlines_flights.conf > x
 cat fleet-info/*BAW-ac.conf ./Oneworld/airlines/BAW.conf                     > xmltools/BAW.conf
 cat fleet-info/*UAL-ac.conf ./Continental/airlines/UAL.conf                  > xmltools/UAL.conf
 cat fleet-info/*DAL-ac.conf ./SkyTeam/airlines/DAL.conf                      > xmltools/DAL.conf
+cat fleet-info/*COA-ac.conf ./StarAlliance/airlines/COA.conf                 > xmltools/COA.conf
+cat fleet-info/*USA-ac.conf ./StarAlliance/airlines/USA.conf                 > xmltools/USA.conf
 
 cd xmltools
 ./conf2xml.pl
 
-for letter in A B D U; do
+for letter in A B C D U; do
     mv ${letter}??.xml ~/src/flightgear-git/fgdata/AI/Traffic/${letter}
 done;
 
